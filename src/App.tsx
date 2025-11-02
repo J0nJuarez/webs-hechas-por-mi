66a73c8e9a4e6eedf1fe1379fbf41e68452989fb
jon <jonjuarez.contacto@gmail.com>
2025-11-02 13:20:38 +0100

    posicionar el portatil

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,15 @@ const Laptop: React.FC<LaptopProps> = () => {
     loader.load('/model/laptop.glb', (gltf) => {
         const scene = gltf.scene
         scene.scale.set(5, 5, 5)
-        scene.position.set(0, 0, 0)
-        scene.rotation.set(0,-0.5, 0)
+        scene.position.set(0, 0, 0) 
 
         // centrar el modelo automáticamente
         const box = new THREE.Box3().setFromObject(scene)
         const center = box.getCenter(new THREE.Vector3())
         scene.position.sub(center)
+        
+        // Ajusta la posición final después de centrar
+        scene.position.add(new THREE.Vector3(0.4, 0.3, -0.5))
 
         setModel(scene)
     })
@@ -48,15 +50,15 @@ const App: React.FC = () => {
         </div>
         <div id="render" className="col-span-3 row-span-3">
             <Canvas
-              camera={{ position: [0, 1, 3], fov: 50 }}
-              resize={{ scroll: false }}
+              camera={{ position: [1, 1, 3], fov: 50 }}
+              resize={{ scroll: true }}
                 style={{ height: '100dvh', width: '100dvw', position: 'absolute', top: 0, left: 0 }}
             >
               <directionalLight position={[0.9, 4, 0.4]} intensity={7.4} color="#808080" />
               <OrbitControls
                 enableZoom={true}
-                enablePan={true}
-                enableRotate={false} // Cambia esto a false para desactivar el drag/rotación
+                enablePan={false}
+                enableRotate={true} 
                 target={[0,0,0]}
                 minDistance={2}
                 maxDistance={10}
