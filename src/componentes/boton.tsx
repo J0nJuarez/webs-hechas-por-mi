cb5954e6273e9e29c4e7745c1ba6b8e4d1d7b1ea
jon <jonjuarez.contacto@gmail.com>
2025-11-02 14:12:50 +0100

    Añadir boton

--- src/componentes/boton.tsx
+++ src/componentes/boton.tsx
@@ -3,18 +3,24 @@ import './boton.css'
 
 interface BotonProps {
     logoUrl?: string;
+    webTitle?: string;
     onClick?: () => void;
 }
 
-const Boton: React.FC<BotonProps> = ({ logoUrl, onClick }) => {
+const Boton: React.FC<BotonProps> = ({ logoUrl, onClick, webTitle }) => {
   return (
-  <button className="btn-small" onClick={onClick}>
-  <div className="btn-small-content">
-    <div className="btn-small-icon">
+  <button onClick={onClick}>
+  <div className="liquidGlass-wrapper button">
+          <div className="liquidGlass-effect"></div>
+          <div className="liquidGlass-tint"></div>
+          <div className="liquidGlass-shine"></div>
+          <div className="liquidGlass-text">
+    <div className="btn-icon">
       {logoUrl && <img src={logoUrl} className='' alt="Logo" />}
     </div>
-    <h2 className="btn-small-text">Ver Web</h2>
+    <h2>Ver Web {webTitle}</h2>
   </div>
+    </div>
 </button>
 
   )
